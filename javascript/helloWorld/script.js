87f30e3d97b425afc2e7fceed96dc90ed1df0edb
Tommy Han <tommy@Tommys-Air.home>
2020-04-03 19:31:42 -0400

    add input box.

--- javascript/helloWorld/script.js
+++ javascript/helloWorld/script.js
@@ -1,5 +1,13 @@
-var button = document.getElementsByTagName("button")[0];
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function(){
+//     console.log("CLICK !!!!");
+// })
+
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
 
 button.addEventListener("click", function(){
-    console.log("CLICK !!!!");
+    console.log("click is working");
 })
