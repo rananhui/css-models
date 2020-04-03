ee97054f8d84b4386a7c4f5f8052c19c45a38473
Tommy Han <tommy@Tommys-Air.home>
2020-04-03 19:53:50 -0400

    dry

--- javascript/helloWorld/script.js
+++ javascript/helloWorld/script.js
@@ -9,9 +9,29 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
-button.addEventListener("click", function(){
-    console.log(input.value);
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
-})
+    input.value = "";
+}
+
+
+function addListAfterClick(){
+    if (inputLength() > 0){
+        createListElement();
+    }
+}
+
+function addListAfterKeyPress(){
+    if (inputLength() > 0 && event.keyCode == 13){
+        createListElement();
+    }
+}
+
+button.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress",addListAfterKeyPress);
