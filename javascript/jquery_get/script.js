b0a4c77472a2d5892a008915b20c4b8dc105927b
Tommy Han <tommy@Tommys-Air.home>
2020-04-04 08:41:18 -0400

    improving display of data.

---
+++ javascript/jquery_get/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $.get("https://image-server-codesmith.firebaseapp.com/images/", function(data, status){
+    //   alert("Data: " + data + "\nStatus: " + status);
+        console.log(`data[0] = ${data[0]}`);
+        console.log(`data[1] = ${data[1]}`);
+        $('p').append(getListOfData(data));
+    });
+  });
+});
+
+
+const getListOfData = (data) => {
+    const urls = data.map((currVal) => `<li>${currVal}</li>`).join("\n");
+    return `<ul>${urls}</ul>`;
+};
--- javascript/promises/index.js
+++ javascript/promises/index.js
@@ -4,9 +4,8 @@ main.innerHTML = "<p>Loading...";
 fetch(URL)
   .then((response) => response.json())
   .then((people) => main.innerHTML = getListOfNames(people));
+
 const getListOfNames = (people) => {
-  const names = people
-    .map((person) => `<li>${person.name}</li>`)
-    .join("\n");
-return `<ul>${names}</ul>`;
+    const names = people.map((person) => `<li>${person.name}</li>`).join("\n");
+    return `<ul>${names}</ul>`;
 };
